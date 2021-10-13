bd35ea7d23c86401198f099c9d15544c3fe1dd6f
StremCipher <47041923+StremCipher@users.noreply.github.com>
2021-10-14 00:45:26 +0530

    first commit

--- netutil/urls.py
+++ netutil/urls.py
@@ -22,6 +22,8 @@ from netutil.netutil import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("Home.urls")),
+
     url(r'^media/(?P<path>.*)$', serve,
         {'document_root':       settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,
